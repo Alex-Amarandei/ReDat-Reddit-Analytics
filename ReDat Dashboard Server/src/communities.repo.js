1e7d3c9a5e8b4c170e586bd573434626149e5c32
ioanapelinn <75810645+ioanapelinn@users.noreply.github.com>
2021-06-06 18:55:38 +0300

    Updated

---
+++ ReDat Dashboard Server/src/communities.repo.js
@@ -0,0 +1,59 @@
+var con = require('./db');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
+async function getCommunityPosts(filterType, redditToken, communityName) {
+    return new Promise((resolve, reject) => {
+        console.log(filterType, redditToken, communityName);
+        var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance 
+        var theUrl = `https://oauth.reddit.com/r/${communityName}/${filterType}.json?limit=50`;
+        xmlhttp.open("GET", theUrl);
+        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xmlhttp.setRequestHeader('Authorization', `bearer ${redditToken}`)
+        xmlhttp.send(null);
+        xmlhttp.onreadystatechange = () => {
+            if (xmlhttp.readyState !== 4) return;
+
+            // Process our return data
+            if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
+                // What do when the request is successful
+                if (xmlhttp.responseText) {
+                    // parse res body
+                    const response = JSON.parse(xmlhttp.responseText);
+                    resolve(response.data.children)
+
+                }
+                // console.log('success', JSON.parse(xmlhttp.responseText));
+            } else {
+                // What to do when the request has failed
+                reject(xmlhttp);
+            }
+        }
+
+    })
+}
+
+async function getCommunitiesByUser(userId) {
+    return new Promise((resolve, reject) => {
+        const retrieveUserSql = "SELECT community_name from users_communities where user_id = ?";
+
+        con.query(retrieveUserSql, [userId], (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        })
+
+    })
+
+}
+
+
+const Communities = function() {}
+
+Communities.prototype.getCommunitiesByUser = getCommunitiesByUser
+Communities.prototype.getCommunityPosts = getCommunityPosts
+
+
+
+module.exports = new Communities()

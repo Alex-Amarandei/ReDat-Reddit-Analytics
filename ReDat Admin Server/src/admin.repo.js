cdad5f2c2d69e811d4484757d6d6a3fba6d7e96f
ioanapelinn <75810645+ioanapelinn@users.noreply.github.com>
2021-06-09 18:29:02 +0300

    Created Admin Server

---
+++ ReDat Admin Server/src/admin.repo.js
@@ -0,0 +1,41 @@
+var con = require('./db');
+
+
+
+async function getUsers() {
+    return new Promise((resolve, reject) => {
+        const retrieveUsersSql = "SELECT * FROM users";
+
+        con.query(retrieveUsersSql, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        })
+
+    })
+
+}
+
+async function banUser(userId) {
+    return new Promise((resolve, reject) => {
+        const retrieveUsersSql = "UPDATE users SET is_banned = 1 WHERE id = ?";
+
+        con.query(retrieveUsersSql, [userId], (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        })
+
+    })
+
+}
+
+const Admin = function() {}
+
+Admin.prototype.getUsers = getUsers
+Admin.prototype.banUser = banUser
+
+
+module.exports = new Admin()
